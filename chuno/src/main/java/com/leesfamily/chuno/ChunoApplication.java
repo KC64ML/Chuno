de3c241603be9e87e7dc199649f65dbe29740db2
leekyoungchang <lkc263@naver.com>
2023-01-17 13:35:48 +0900

    [feat] springboot project initializer

---
+++ chuno/src/main/java/com/leesfamily/chuno/ChunoApplication.java
@@ -0,0 +1,13 @@
+package com.leesfamily.chuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChunoApplication.class, args);
+	}
+
+}
---
+++ chuno/src/test/java/com/leesfamily/chuno/ChunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leesfamily.chuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
