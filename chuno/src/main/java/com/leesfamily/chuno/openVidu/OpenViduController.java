3b53a2332451e0d08386df909bc95923bed28151
이주찬 <ljc9393@nate.com>
2023-02-02 13:26:46 +0900

    [Back-fix] requestMapping 추가...

--- chuno/src/main/java/com/leesfamily/chuno/openVidu/OpenViduController.java
+++ chuno/src/main/java/com/leesfamily/chuno/openVidu/OpenViduController.java
@@ -9,16 +9,13 @@ import io.openvidu.java.client.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @CrossOrigin
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class OpenViduController {
 
     @Value("${OPENVIDU_URL}")
