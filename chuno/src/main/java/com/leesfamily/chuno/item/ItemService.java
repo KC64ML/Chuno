653d80c3d511558880f630d9630278564524026b
이주찬 <ljc9393@nate.com>
2023-02-02 09:40:38 +0900

    [feat] 상점용 아이템 Api

--- chuno/src/main/java/com/leesfamily/chuno/item/ItemService.java
+++ chuno/src/main/java/com/leesfamily/chuno/item/ItemService.java
@@ -1,7 +1,26 @@
 package com.leesfamily.chuno.item;
 
+import com.leesfamily.chuno.item.model.ItemEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class ItemService {
+
+    final private ItemRepository itemRepository;
+
+    public List<ItemEntity> getItemList() {
+        List<ItemEntity> list = itemRepository.findAll();
+        return list;
+    }
+
+    public List<ItemEntity> getTypeItemList(int forRunner) {
+        List<ItemEntity> list = itemRepository.findByForRunner(forRunner);
+        return list;
+    }
+
 }
