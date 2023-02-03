a52590fafb03ed6fb36fb8fb99483d03c622011b
이동하 <siganshoyou@gmail.com>
2023-02-03 12:09:56 +0900

    [front-fix]merge_conflict

--- real/src/views/GameView.vue
+++ real/src/views/GameView.vue
@@ -1,47 +1,54 @@
 <template>
   <div>
     <OpenViduVue style="z-index: 100;"></OpenViduVue>
-    <MapView/>
-    <MenuView v-if="menu"/>
-  
-    <GameFooterView 
-      class="footer_fix"
-      @on-menu="onMenu"
-    />
+    <MapView />
+    <MenuView v-if="menu" />
+
+    <div id="footer_container">
+      <div class="menu_box flex_center" @click="this.$router.push('/home')">
+        <img class="menu" src="@/assets/game_chat.png">
+      </div>
+      <div>
+        <!-- 채팅창 수정 필요 -->
+        <input class="map_search" type="text" placeholder="채팅을 입력해 주세요">
+      </div>
+      <div class="menu_box" @click="myCam">
+        <img class="menu" src="@/assets/game_myCam.png">
+      </div>
+      <div class="menu_box" @click="onMenu">
+        <img class="menu" src="@/assets/game_menu.png">
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
 import OpenViduVue from '@/components/game/OpenViduVue.vue'
 import MapView from '@/components/game/MapView.vue'
-import GameFooterView from "@/components/GameFooterView.vue"
 import MenuView from '@/components/game/MenuView.vue'
 
 export default {
 
   name: 'GameView',
   components: {
-    GameFooterView,
     MapView,
     OpenViduVue,
     MenuView,
   },
-<<<<<<< HEAD
-  data(){
+  data() {
     return {
       menu: true,
     }
   },
   methods: {
-    onMenu(){
+    onMenu() {
       console.log('menu clicked')
       this.menu = !this.menu
-=======
-  methods: {
-    testa(ttt) {
-      console.log(ttt);
->>>>>>> 4e1c161a5b72d35b7e3827b59fe4f11d906e460d
-    }
+    },
+    myCam() {
+      alert('myCam눌림')
+    },
   }
 }
 
@@ -49,6 +56,46 @@ export default {
 
 <style lang="scss" scoped>
 @import "@/assets/scss/variable.scss";
-  
+$button_width: 60px;
 
+#footer_container {
+  position: absolute;
+  bottom: 0;
+  background-color: black;
+  height: $footer_height;
+  width: 100vw;
+  display: grid;
+  grid-template-columns: $button_width 1fr $button_width $button_width;
+  justify-content: space-around;
+  align-items: center;
+}
+
+#footer_container>div {
+  width: 100%;
+}
+
+// .menu {
+//     height: 25px;
+// }
+.menu_box {
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.map_search {
+  display: block;
+  margin: 0 auto;
+  width: 95%;
+  height: 40px;
+  background-color: #F1F1F180;
+  color: white;
+  padding: 0 10px;
+  border-radius: 15px;
+  font-size: 20px;
+}
+.map_search::placeholder {
+  color: rgba(255, 255, 255, 0.56)
+}
 </style>
