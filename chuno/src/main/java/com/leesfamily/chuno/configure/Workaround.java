8cef1a1abc4c50de094ad621a637daf873de873a
lkc263 <lkc263@naer.com>
2023-02-05 22:48:19 +0900

    [backend-fix] swagger port 추가

--- chuno/src/main/java/com/leesfamily/chuno/configure/Workaround.java
+++ chuno/src/main/java/com/leesfamily/chuno/configure/Workaround.java
@@ -2,6 +2,7 @@ package com.leesfamily.chuno.configure;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.stereotype.Component;
 import springfox.documentation.oas.web.OpenApiTransformationContext;
 import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
 import springfox.documentation.spi.DocumentationType;
@@ -9,6 +10,7 @@ import springfox.documentation.spi.DocumentationType;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 
+@Component
 public class Workaround implements WebMvcOpenApiTransformationFilter {
     @Override
     public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
