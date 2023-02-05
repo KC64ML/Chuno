84589c33939a3dcb96986937dab67d18c38ff51d
lkc263 <lkc263@naer.com>
2023-02-05 21:47:15 +0900

    [backend-feat] "http" or "https" for CORS request. 적용 테스트

--- chuno/src/main/java/com/leesfamily/chuno/configure/SwaggerConfiguration.java
+++ chuno/src/main/java/com/leesfamily/chuno/configure/SwaggerConfiguration.java
@@ -3,8 +3,10 @@ package com.leesfamily.chuno.configure;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -81,5 +83,16 @@ public class SwaggerConfiguration {
                         .version("v0.0.1"))
                         .addSecurityItem(securityRequirement)
                         .components(components);
+    }
+
+
+    // https로 접속가능하게
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addServersItem(new Server().url("/"))
+                .components(new Components().addSecuritySchemes("basicScheme",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info(new Info().title("SpringShop API").version("V1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
