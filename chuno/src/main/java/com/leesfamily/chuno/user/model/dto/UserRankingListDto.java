56f10107479f9b128d39d085bb2348d16b1527d0
이주찬 <ljc9393@nate.com>
2023-02-09 10:44:56 +0900

    [Back-fix] 유저 랭킹에 Id 값 추가

--- chuno/src/main/java/com/leesfamily/chuno/user/model/dto/UserRankingListDto.java
+++ chuno/src/main/java/com/leesfamily/chuno/user/model/dto/UserRankingListDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 public class UserRankingListDto {
 
     // 넘겨야할 값 : 닉네임(nickname), 레벨(level), 노비승(runnerWinCount), 추노승(chaserWinCount), 승률( (runnerWinCount + chaserWinCount) / (runnerPlayCount + chaserPlayCount) * 100 : int 형으로
-    
+    Long id;
     String nickname; // 닉네임
     int level; // 레벨
     int runnerWinCount; // 노비승
@@ -17,7 +17,8 @@ public class UserRankingListDto {
     int userCountAvg; // 승률
 
     @QueryProjection
-    public UserRankingListDto(String nickname, int level, int runnerWinCount, int chaserWinCount, int userCountAvg) {
+    public UserRankingListDto(Long id, String nickname, int level, int runnerWinCount, int chaserWinCount, int userCountAvg) {
+        this.id = id;
         this.nickname = nickname;
         this.level = level;
         this.runnerWinCount = runnerWinCount;
