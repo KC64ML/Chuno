595e9ddd9bdf8561294511256d3db516db0eb74f
lkc263 <lkc263@naver.com>
2023-02-11 22:11:15 +0900

    [Front-fix] 친구 검색 오류 해결 및 회원가입 화면 수정중

--- real/src/views/RegisterView.vue
+++ real/src/views/RegisterView.vue
@@ -1,46 +1,51 @@
 <template>
-  <div id="main_logo">
-    <img src="@/assets/main_logo2.png" alt="">
-  </div>
-  <div id="container">
-    <div style="font-size: 25px;">
-      프로필 입력
+  <div>
+    <div class="regiterview-header" id="main_logo">
+      <img src="@/assets/main_logo2.png" alt="">
     </div>
-    <div id="profile_image">
-      <div v-if="this.img_url">
-        <div id="profile_background">
-          <img id="preview_img" :src="this.img_url"/>
+    
+    <div class="regiterview">
+      <div class="container-col" id="container">
+        <div class="header_menu" style="font-size: 25px;">
+          프로필 입력
         </div>
+        <div class="header_menu" id="profile_image">
+          <div v-if="this.img_url">
+            <div id="profile_background">
+              <img id="preview_img" :src="this.img_url" />
+            </div>
+            <div>
+              <button @click="clearImage">다시 선택할래요</button>
+            </div>
+          </div>
+          <div v-else>
+            <img id="blank_img" src="@/assets/profile_default_with_cam.svg" alt="" @click="profile_click">
+          </div>
+          <input ref="file_input" type="file" @change="oneFileSelect" style="display:none" />
+        </div>
+        <div id="input_box" style="display: flex; align-items: center;">
+          <div style="margin-right: 19px;">닉네임</div>
+          <div>
+            <input id="nickname_input" ref="nickname_input" v-model="nickname" maxlength="6">
+          </div>
+        </div>
+        <div v-if="this.nickname.length">
+          <div v-if="!can_use" style="color: red;">이미 사용 중이에요</div>
+          <div v-else style="color: green;">사용 할 수 있어요</div>
+        </div>
+        <div id="input_box" style="display: flex; align-items: center;">
+          <div style="margin-right: 5px;">전화번호</div>
+          <div>
+            <input id="nickname_input" ref="nickname_input" v-model="phone" maxlength="12">
+          </div>
+        </div>
+        <div class="notice grey">전화번호는 '-' 없이 숫자만 입력해주세요.</div>
         <div>
-          <button @click="clearImage" >다시 선택할래요</button>
+          <button id="save_button" @click="save">저장</button>
         </div>
       </div>
-      <div v-else>
-        <img id="blank_img" src="@/assets/profile_default_with_cam.svg" alt="" @click="profile_click">
-      </div>
-      <input ref="file_input" type="file" @change="oneFileSelect" style="display:none"/>
     </div>
-    <div id="input_box" style="display: flex; align-items: center;">
-      <div style="margin-right: 19px;">닉네임</div>
-      <div>
-        <input id="nickname_input" ref="nickname_input" v-model="nickname" maxlength="6">
-      </div>
-    </div>
-    <div v-if="this.nickname.length">
-      <div v-if="!can_use" style="color: red;">이미 사용 중이에요</div>
-      <div v-else style="color: green;">사용 할 수 있어요</div>
-    </div>
-    <div id="input_box" style="display: flex; align-items: center;">
-      <div style="margin-right: 5px;">전화번호</div>
-      <div>
-        <input id="nickname_input" ref="nickname_input" v-model="phone" maxlength="12">
-      </div>
-    </div>
-      <div class="notice grey">전화번호는 '-' 없이 숫자만 입력해주세요.</div>
-    <div>
-      <button id="save_button" @click="save">저장</button>
-    </div>
-  </div>
+</div>
 </template>
 
 <script>
@@ -61,10 +66,10 @@ export default {
     check() {
       console.log(this.nickname);
       this.axios.get(process.env.VUE_APP_SPRING + "user/nickname/" + this.nickname)
-        .then(({data})=>{
-          if(data.code) {
+        .then(({ data }) => {
+          if (data.code) {
             this.can_use = false;
-          }else {
+          } else {
             this.can_use = true;
           }
         })
@@ -93,25 +98,25 @@ export default {
         formData.append("file", this.one_file);
       }
 
-      this.axios.post(process.env.VUE_APP_SPRING + "kakao/register", formData, { 
-        headers: { 
-          'Content-Type': 'multipart/form-data', 
+      this.axios.post(process.env.VUE_APP_SPRING + "kakao/register", formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
         }
       })
-        .then((res)=>{
+        .then((res) => {
           // const code = res.data.code
           // if(code) {
-            sessionStorage.setItem('token', res.data)
-            console.log('회원가입 성공')
-            console.log(res)
-            alert("등록완료");
-            this.$router.push({ name: "home" });
+          sessionStorage.setItem('token', res.data)
+          console.log('회원가입 성공')
+          console.log(res)
+          alert("등록완료");
+          this.$router.push({ name: "home" });
           // } else {
           //   console.log(res)
           //   console.log('code err')
           // }
         })
-        .catch((e)=>{
+        .catch((e) => {
           console.log('회원가입 실패')
           console.log(e)
         })
@@ -126,7 +131,7 @@ export default {
       this.img_url = undefined;
     }
   },
-  
+
   watch: {
     'nickname': 'check'
   },
@@ -144,17 +149,20 @@ $button_height: 40px;
 $logo_size: 120px;
 $image_size: 140px;
 $div_interval: 40px;
+
 #main_logo {
   position: absolute;
   top: 60px;
 }
-#main_logo > img {
+
+#main_logo>img {
   width: $logo_size;
 }
 
-#blank_img{
+#blank_img {
   width: $image_size;
 }
+
 #container {
   transform: translateY(-5%);
 }
@@ -162,9 +170,11 @@ $div_interval: 40px;
 #container div {
   text-align: center;
 }
+
 #input_box {
   margin-top: $div_interval;
 }
+
 #nickname_input {
   background-color: rgba(67, 64, 57, 0.3);
   height: $input_height;
@@ -172,6 +182,7 @@ $div_interval: 40px;
   border-radius: $input_height / 2;
   color: #1D182C;
 }
+
 #save_button {
   margin-top: $div_interval;
   background-color: #1D182C;
@@ -181,6 +192,7 @@ $div_interval: 40px;
   border-radius: $button_height / 2;
   font-size: 20px;
 }
+
 #profile_background {
   // background-color: rgb(24, 138, 28);
   margin: 20px auto;
@@ -192,16 +204,26 @@ $div_interval: 40px;
   justify-content: center;
   overflow: hidden;
 }
+
 #preview_img {
   width: 100%;
   height: 100%;
-  
 }
+
 .notice {
   margin: 20px;
 }
+
 .grey {
   color: gray;
 }
 
+.regiterview {
+  width: vw;
+}
+
+.regiterview-header {
+  position: absolute;
+  top: 70px;
+}
 </style>
