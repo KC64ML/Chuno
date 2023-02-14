351e4b3e8944fd20efb2e04d6c2c8beffc3663f3
이주찬 <ljc9393@nate.com>
2023-02-14 16:35:24 +0900

    [front-fix] dummy data

--- chuno/src/main/resources/data.sql
+++ chuno/src/main/resources/data.sql
@@ -1,22 +1,16 @@
-INSERT INTO users
-(email, nickname, reg_dt, runner_play_count, runner_win_count, chaser_play_count, chaser_win_count)
-VALUES
-    ('chaen511@naver.com', '성곡초짱이채은', NOW(), 10, 3, 9, 2),
-    ('ljc1025@nate.com', '노예킹', NOW(), 20, 10, 4, 1),
-    ('lkc263@naver.com', '아리따움', NOW(), 40, 30, 20, 15),
-    ('siganshoyou@google.com', '추노장인', NOW(), 30, 15, 10, 6);
+
 insert into users
 (reg_dt, catch_count, chaser_play_count, chaser_win_count, email, exp, is_deleted, is_manager, level, money, nickname, paper_count, path, save_name, runner_play_count, runner_win_count, phone)
 values
-(now(), 0, 11, 7, "ljc9393@nate.com", 0, 0, 0, 1, 0, "큰개님", 0, null, null, 30, 15, "01051411025"),
-(now(), 0, 11, 7, "opi6@hanmail.net", 0, 0, 0, 1, 0, "인의동큰손", 0, null, null, 30, 15, "01026896940"),
-(now(), 0, 11, 7, "lce511@naver.com", 0, 0, 0, 1, 85900, "채은짱님", 0, null, null, 30, 15, "01084054759"),
-(now(), 0, 11, 7, "souk0712@naver.com", 0, 0, 0, 1, 80000, "모카", 0, null, null, 30, 15, "01082860799"),
-(now(), 0, 11, 7, "asdf@naver.com", 0, 0, 0, 1, 0, "아무", 0, null, null, 13, 15, "01515498451"),
-(now(), 0, 11, 7, "qwer@naver.com", 0, 0, 0, 1, 524, "이름", 0, null, null, 51, 15, "01059519858"),
-(now(), 0, 11, 7, "zxcv@naver.com", 0, 0, 0, 1, 5000, "짓기", 0, null, null, 30, 15, "01098751351"),
-(now(), 0, 11, 7, "grg@naver.com", 0, 0, 0, 1, 800, "귀찮아죽겠음", 0, null, null, 30, 15, "01849878512"),
-(now(), 0, 11, 7, "tnth@naver.com", 0, 0, 0, 1, 7000, "닉넴뭐하지", 0, null, null, 30, 15, "01012168849");
+(now(), 0, 11, 7, "ljc9393@nate.com", 0, 0, 0, 1, 0, "큰개님", 0, "", "", 30, 15, "01051411025"),
+(now(), 0, 11, 7, "opi6@hanmail.net", 0, 0, 0, 1, 0, "인의동큰손", 0, "", "", 30, 15, "01026896940"),
+(now(), 0, 11, 7, "lce511@naver.com", 0, 0, 0, 1, 85900, "채은짱님", 0, "", "", 30, 15, "01084054759"),
+(now(), 0, 11, 7, "souk0712@naver.com", 0, 0, 0, 1, 80000, "모카", 0, "", "", 30, 15, "01082860799"),
+(now(), 0, 11, 7, "asdf@naver.com", 0, 0, 0, 1, 0, "아무", 0, "", "", 13, 15, "01515498451"),
+(now(), 0, 11, 7, "qwer@naver.com", 0, 0, 0, 1, 524, "이름", 0, "", "", 51, 15, "01059519858"),
+(now(), 0, 11, 7, "zxcv@naver.com", 0, 0, 0, 1, 5000, "짓기", 0, "", "", 30, 15, "01098751351"),
+(now(), 0, 11, 7, "grg@naver.com", 0, 0, 0, 1, 800, "귀찮아죽겠음", 0, "", "", 30, 15, "01849878512"),
+(now(), 0, 11, 7, "tnth@naver.com", 0, 0, 0, 1, 7000, "닉넴뭐하지", 0, "", "", 30, 15, "01012168849");
 
 
 INSERT INTO rooms
