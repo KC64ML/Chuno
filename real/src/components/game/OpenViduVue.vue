a678e204bc81f111c7d56b934eec1f5ce92993b5
opi6opi6 <opi6@hanmail.net>
2023-02-21 01:26:38 +0900

    [front=fix]오디오끔

--- real/src/components/game/OpenViduVue.vue
+++ real/src/components/game/OpenViduVue.vue
@@ -141,7 +141,7 @@ const APPLICATION_SERVER_URL = process.env.VUE_APP_RTC;
                             audioSource: undefined, // The source of audio. If undefined default microphone
                             videoSource: this.myVideoStream, // The source of video. If undefined default webcam
                             publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
-                            publishVideo: true, // Whether you want to start publishing with your video enabled or not
+                            publishVideo: false, // Whether you want to start publishing with your video enabled or not
                             resolution: "640x480", // The resolution of your video
                             frameRate: 60, // The frame rate of your video
                             insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
