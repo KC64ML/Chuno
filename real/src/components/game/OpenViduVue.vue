4208e480b0538d484a8996f9db62472e7e7bda94
opi6opi6 <opi6@hanmail.net>
2023-02-21 01:18:37 +0900

    sefes

--- real/src/components/game/OpenViduVue.vue
+++ real/src/components/game/OpenViduVue.vue
@@ -142,8 +142,8 @@ const APPLICATION_SERVER_URL = process.env.VUE_APP_RTC;
                             videoSource: this.myVideoStream, // The source of video. If undefined default webcam
                             publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
                             publishVideo: true, // Whether you want to start publishing with your video enabled or not
-                            resolution: "64x48", // The resolution of your video
-                            frameRate: 2, // The frame rate of your video
+                            resolution: "640x480", // The resolution of your video
+                            frameRate: 60, // The frame rate of your video
                             insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
                             mirror: true, // Whether to mirror your local video or not
                         });
